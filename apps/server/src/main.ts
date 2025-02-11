a1d4e43f0a35431a3205db6a769f0359e9dc022c
Christian <paul.chris.dev@gmail.com>
2025-02-11 17:38:31 +0800

    enable server cors

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -4,6 +4,7 @@ import 'reflect-metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors()
   await app.listen(process.env.API_PORT ?? 3000);
 }
 bootstrap();
