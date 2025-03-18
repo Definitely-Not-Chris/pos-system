ac86200f77dc200276cb58f688f0510afdce287f
Christian <paul.chris.dev@gmail.com>
2025-03-18 14:42:15 +0800

    added allowed hosts

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    allowedHosts: ['ec2-13-201-226-233.ap-south-1.compute.amazonaws.com']
+    allowedHosts: ['ec2-13-201-226-233.ap-south-1.compute.amazonaws.com'],
     port: 80
   }
 })
